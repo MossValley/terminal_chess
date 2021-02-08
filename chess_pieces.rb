182bdb9f0f49702bebb0b2304d9dbdc2582d792a
MossValley <ramivalmont@gmail.com>
2021-02-08 21:50:23 +1300

    Add some functions to ChessPiece to handle movement
    Adjusted some of the input parameters and adjusted the spec test accordingly

--- chess_pieces.rb
+++ chess_pieces.rb
@@ -2,42 +2,85 @@ require 'pry'
 require './chess_board.rb'
 
 class ChessPiece
-  def initialize(icon = "x", start=[7, 1])
+  attr_reader :is_white, :current_node
+  
+  def initialize(icon = "x", node=nil, is_white=true)
     @icon = icon
-    @current_position = start
-    @current_x = @current_position[0].nil? ? nil : @current_position[0]
-    @current_y = @current_position[-1].nil? ? nil : @current_position[-1]
+    @current_node = node
+    @is_white = is_white
+    @move_to_node = @current_node
 
-    @board_space = Board.new.possible_moves
+    # @current_position = @current_node.nil? ? nil : @current_node.data
+    # @current_x = @current_position[0].nil? ? nil : @current_position[0]
+    # @current_y = @current_position[-1].nil? ? nil : @current_position[-1]
+
+    update_position
+
+    @move_set = nil
+    @attack_set = nil
+
   end
   
   def self
     p "#{@icon}\n#{@current_position}"
   end
 
-  def spaces
-    p @board_space
+  def taken
+    piece_is_taken
   end
 
-  def taken
-    #code
+  def move_this_piece(new_node)
+    @move_to_node = new_node
+    check_path
   end
 
   private
 
+  def update_position
+    @current_node = @move_to_node
+    @current_position = @current_node.nil? ? nil : @current_node.data
+    @current_x = @current_position[0].nil? ? nil : @current_position[0]
+    @current_y = @current_position[-1].nil? ? nil : @current_position[-1]
+  end
+
   def piece_is_taken
-    @current_position = nil
-    
+    @move_to_node = nil
+    update_position
+  end
+
+  def check_path
+    if @move_set.include?(@move_to_node)
+      valid_move
+    else
+      "Move not valid"
+    end
+  end
+
+  def valid_move
+    if !@move_to_node.is_occupied #i.e. node is not occupied
+      update_position
+    else
+      if @attack_set.include?(@move_to_node) && (self.is_white ? @move_to_node.piece.is_white : !@move_to_node.piece.is_white)
+        attack_enemy
+      else
+      "Position occupied by friendly piece"
+      end
+    end
+  end
+
+  def attack_enemy
+    @move_to_node.piece.piece_is_taken
+    update_position
   end
 
 end
 
-class Pawn < ChessPiece
+class WhitePawn < ChessPiece
   attr_reader :move_set, :attack_set
   
-  def initialize (icon = "p", start=[7, 1])
+  def initialize (icon="p", node=nil, is_white=true)
     super
-    @start_possition = @current_position
+    @start_position = @current_position
     @move_set = pawn_moves
     @attack_set = pawn_attack_moves
   end
@@ -47,7 +90,7 @@ class Pawn < ChessPiece
   def pawn_moves  
     moves = []
 
-    if @current_position == @start_possition
+    if @current_position == @start_position
       move = [(@current_x -2), @current_y]
       moves << move
     end
--- spec/chess_pieces_spec.rb
+++ spec/chess_pieces_spec.rb
@@ -1,35 +1,43 @@
 require "./chess_pieces"
 
 describe ChessPiece do
-  let(:piece) { ChessPiece.new("X", "7,1") }
+  before do 
+    @node = double("BoardNode")
+    allow(@node).to receive(:data) {[7, 1]}
+    @piece = ChessPiece.new("X", @node)
+  end
 
   describe "self" do
     it "prints its icon and start position" do
       allow($stdout).to receive(:write)
-      expect(piece.self).to eql("X\n7,1")
+      expect(@piece.self).to eql("X\n[7, 1]")
     end
   end
 end
 
-describe Pawn do
-  let(:pawn) { Pawn.new }
+describe WhitePawn do
+  before do 
+    @node = double("BoardNode")
+    allow(@node).to receive(:data) {[7, 1]}
+    @w_pawn = WhitePawn.new("p", @node)
+  end
 
   describe "#self" do
     it "prints its icon and start position" do
       allow($stdout).to receive(:write)
-      expect(pawn.self).to eql("p\n[7, 1]")
+      expect(@w_pawn.self).to eql("p\n[7, 1]")
     end
   end
 
   describe "@move_set" do
     it "shows possible moves from start position" do
-      expect(pawn.move_set).to eql([[5, 1], [6, 1]])
+      expect(@w_pawn.move_set).to eql([[5, 1], [6, 1]])
     end 
   end
 
   describe "@attack_set" do
     it "shows possibe moves to attack" do
-      expect(pawn.attack_set).to eql([[6, 0], [6, 2]])
+      expect(@w_pawn.attack_set).to eql([[6, 0], [6, 2]])
     end
   end
 
