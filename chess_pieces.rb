9e75dcc174b967374b6e808341862bba8cbe390a
MossValley <ramivalmont@gmail.com>
2021-02-11 22:44:52 +1300

    Refactor #pawn_moves
    Also refactor #pawn_move and #pawn_attack

--- chess_pieces.rb
+++ chess_pieces.rb
@@ -95,73 +95,59 @@ class WhitePawn < ChessPiece
 
   private 
 
-  def pawn_moves(temp_node=@current_node)
-    temp_x = temp_node.data[0]
-    temp_y = temp_node.data[-1]
-    
-    new_node_x = @move_to_node.data[0]
-    new_node_y = @move_to_node.data[-1]
+  def pawn_moves    
+    move_x = @move_to_node.data[0]
+    move_y = @move_to_node.data[-1]
 
-    x_is_same = new_node_x == temp_x ? true : false
-    y_is_same = new_node_y == temp_y ? true : false
+    x_is_same = move_x == @current_x ? true : false
+    y_is_same = move_y == @current_y ? true : false
 
     #initial move - can move two spaces
     if @current_position == @start_position
-      if new_node_x == temp_x -2
-        temp_node = temp_node.up
-        move_again = move_further(temp_node)
+      if move_x == @current_x -2 && y_is_same
+        temp_node = @current_node.up
         if !temp_node.is_occupied
-          temp_node = temp_node.up 
-          pawn_move(temp_node)
+          pawn_move
         else
           "Path blocked"
         end
       end
     end
     #regular move
-    if new_node_x == temp_x -1 && y_is_same
-      temp_node = temp_node.up
-      pawn_move(temp_node)
-    elsif new_node_x > temp_x && y_is_same
+    if move_x == @current_x -1 && y_is_same
+      pawn_move
+    elsif move_x > @current_x && y_is_same
       return "Pawn cannot move down"
     end
     #attack move
-    if new_node_x == temp_x -1 && new_node_y == temp_y -1
-      temp_node = temp_node.up_l
-      pawn_attack(temp_node)
-    elsif new_node_x == temp_x -1 && new_node_y == temp_y +1
-      temp_node = temp_node.up_r
-      pawn_attack(temp_node)
+    if move_x == @current_x -1 && (move_y == @current_y -1 || move_y == @current_y +1)
+      pawn_attack
     end
 
-    return "Move not valid" if new_node_x < temp_x-2 || (new_node_y > temp_y +1 || new_node_y < temp_y -1)
+    return "Move not valid" if move_x < @current_x-2 || (move_y > @current_y +1 || move_y < @current_y -1)
     return "Pawn is at this location" if x_is_same && y_is_same
   end
 
-  def pawn_move(temp_node)
-    if temp_node == @move_to_node
-      if !@move_to_node.is_occupied
-        update_position
+  def pawn_move
+    if !@move_to_node.is_occupied
+      update_position
+    else
+      if (self.is_white ? !@move_to_node.piece.is_white : @move_to_node.piece.is_white)
+        "Enemy unit in position"
       else
-        if (self.is_white ? !@move_to_node.piece.is_white : @move_to_node.piece.is_white)
-          "Enemy unit in position"
-        else
-          "Friendly unit in position"
-        end
+        "Friendly unit in position"
       end
     end
   end
 
-  def pawn_attack(temp_node)
-    if temp_node == @move_to_node
-      if !@move_to_node.is_occupied
-        "Cannot attack empty square"
+  def pawn_attack
+    if !@move_to_node.is_occupied
+      "Cannot attack empty square"
+    else
+      if (self.is_white ? !@move_to_node.piece.is_white : @move_to_node.piece.is_white)
+        attack_enemy
       else
-        if (self.is_white ? !@move_to_node.piece.is_white : @move_to_node.piece.is_white)
-          attack_enemy
-        else
-          "Friendly unit in position"
-        end
+        "Friendly unit in position"
       end
     end
   end
@@ -184,21 +170,21 @@ class Rook < ChessPiece
     temp_x = temp_node.data[0]
     temp_y = temp_node.data[-1]
     
-    new_node_x = @move_to_node.data[0]
-    new_node_y = @move_to_node.data[-1]
+    move_x = @move_to_node.data[0]
+    move_y = @move_to_node.data[-1]
 
-    x_is_same = new_node_x == temp_x ? true : false
-    y_is_same = new_node_y == temp_y ? true : false
+    x_is_same = move_x == temp_x ? true : false
+    y_is_same = move_y == temp_y ? true : false
 
-    if new_node_x < temp_x && y_is_same
+    if move_x < temp_x && y_is_same
       temp_node = temp_node.up
-    elsif new_node_x > temp_x && y_is_same
+    elsif move_x > temp_x && y_is_same
       temp_node = temp_node.down
     end
 
-    if new_node_y < temp_y && x_is_same
+    if move_y < temp_y && x_is_same
       temp_node = temp_node.left
-    elsif new_node_y > temp_y && x_is_same
+    elsif move_y > temp_y && x_is_same
       temp_node = temp_node.right
     end
 
